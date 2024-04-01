5e5fbc6a5967b5c2d823e72b3d7fdc27b72a8e24
Aqsa Shaikh <160129099+Aqsamajeed00@users.noreply.github.com>
2024-04-01 22:12:29 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+"use strict";
+// Assignment Operators
+// let x = 20
+// console.log(x);
+Object.defineProperty(exports, "__esModule", { value: true });
+// let a = 20
+// console.log(x=5);
+//Comparsion Operators 
+// let a = 5
+// let b = 2 
+// let equal = (a == b) // false
+// console.log(equal)
+// let isnotequal = (a !=b) // true
+// console.log(isnotequal)
+// let greaterthen = (a > b) // true
+// console.log(greaterthen)
+// let lowethen = (a < b) // false
+// console.log(lowethen)
+// what is boolean
+let fullNames = "Aqsa Shaikh";
+console.log(fullNames === "Miraal Shaikh");
---
+++ main.ts
@@ -0,0 +1,30 @@
+// Assignment Operators
+// let x = 20
+// console.log(x);
+
+// let a = 20
+// console.log(x=5);
+
+
+//Comparsion Operators 
+// let a = 5
+// let b = 2 
+// let equal = (a == b) // false
+// console.log(equal)
+
+// let isnotequal = (a !=b) // true
+// console.log(isnotequal)
+
+// let greaterthen = (a > b) // true
+// console.log(greaterthen)
+
+// let lowethen = (a < b) // false
+// console.log(lowethen)
+
+// what is boolean
+let fullNames = "Aqsa Shaikh";
+console.log(fullNames === "Miraal Shaikh");
+
+//
+
+
